fbcd7a8f804d71c4cf973028beb1ad9de7387f8b
hacchi-12 <32477854+hacchi-12@users.noreply.github.com>
2019-01-27 16:47:21 +0900

    generalize setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-DOT_FILES=(.bashrc .emacs.d .gitignore .gitconfig)
-
-for file in ${DOT_FILES[@]}
+for file in .??*
 do
+    [[ "$file" == ".git" ]] && continue
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
