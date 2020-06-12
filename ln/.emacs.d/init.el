22aff4b7aa62354bbef72a37c23a3ffb1a1f775a
hacchi-12 <32477854+hacchi-12@users.noreply.github.com>
2020-06-13 00:24:17 +0900

    add settings. hide-bar and hide-message, frame-title, display-time, etc...

--- ln/.emacs.d/init.el
+++ ln/.emacs.d/init.el
@@ -1,13 +1,20 @@
 ;; ++++++++++++++++++++++++++++++++++++++++++++++++++
-;;    etc
+;; etc
 ;; ++++++++++++++++++++++++++++++++++++++++++++++++++
 ;;(global-whitespace-mode 1) ;; visualization of space and tab
 (setq-default tab-width 4 indent-tabs-mode nil) ;; use space on tab
-(setq inhibit-startup-message t)
+(setq inhibit-startup-message t) ;; hide startup message
+(setq initial-scratch-message "") ;; hide *scratch* buffer message
+(menu-bar-mode -1) ;; hide menu bar
+(tool-bar-mode 0) ;; hide tool bar
+(show-paren-mode 1) ;; illuminate corresponding brackets
+(setq frame-title-format "%f") ;; show full path in title
+(display-time-mode t)
 
 ;; windmove setting
 (windmove-default-keybindings) ;; use shift+arrow
 ;;(windmove-default-keybindings 'meta) ;; use alt+arrow
+
 
 
 
