c886cc5adc8204be16ab802059b6fffc4d38987e
miya2804 <32477854+miya2804@users.noreply.github.com>
2020-07-10 04:13:56 +0900

    color change.(font-lock-function-name-face, hl-line, web-mode-html-tag-bracket-face, web-mode-html-tag-face)

--- ln/.emacs.d/init.el
+++ ln/.emacs.d/init.el
@@ -134,5 +134,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(hl-line ((t (:background "color-236")))) ;; hi-line-mode
- )
+ '(font-lock-function-name-face ((t (:foreground "color-208"))))
+ '(hl-line ((t (:background "color-236"))))
+ '(web-mode-html-tag-bracket-face ((t (:foreground "white"))))
+ '(web-mode-html-tag-face ((t (:foreground "brightblue")))))
