137ad216063bbd242e8b46565fe07f48c79f0c6d
miya2804 <32477854+miya2804@users.noreply.github.com>
2020-07-23 12:15:37 +0900

    Fixed not to depend no the directory location. And, Added option -i to ln command.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
 echo dotfiles setting.
 echo
 echo -------------- linking --------------
-for file_path in `\find $HOME/dotfiles/ln -mindepth 1 -maxdepth 1`;
+for file_path in `\find $SCRIPT_DIR/ln -mindepth 1 -maxdepth 1`;
 do
-    read file_name < <(echo $file_path | sed -e "s:^$HOME/dotfiles/ln/::")
+    read file_name < <(echo $file_path | sed -e "s:^$SCRIPT_DIR/ln/::")
     if [ -e $HOME/$file_name ]; then
 	    if [ -d $file_path ]; then
 	        echo "[FAILED] Directory exists: $HOME/$file_name"
@@ -14,18 +16,18 @@ do
 	    fi             
     else
 	    if [ -d $file_path ]; then
-	        ln -s $file_path $HOME/$file_name && echo "[  OK  ] '$file_path/' -> '$HOME/$file_name/'"
+	        ln -si $file_path $HOME/$file_name && echo "[  OK  ] '$file_path/' -> '$HOME/$file_name/'"
 	    else
-	        ln -s $file_path $HOME/$file_name && echo "[  OK  ] '$file_path' -> '$HOME/$file_name'"
+	        ln -si $file_path $HOME/$file_name && echo "[  OK  ] '$file_path' -> '$HOME/$file_name'"
 	    fi
     fi				
 done
 echo
 echo
 echo -------------- copying --------------
-for file_path in `\find $HOME/dotfiles/cp -mindepth 1 -maxdepth 1`;
+for file_path in `\find $SCRIPT_DIR/cp -mindepth 1 -maxdepth 1`;
 do
-    read file_name < <(echo $file_path | sed -e "s:^$HOME/dotfiles/cp/::")
+    read file_name < <(echo $file_path | sed -e "s:^$SCRIPT_DIR/cp/::")
     if [ -e $HOME/$file_name ]; then
 	    if [ -d $file_path ]; then
 	        echo "[FAILED] Directory exists: $HOME/$file_name"
