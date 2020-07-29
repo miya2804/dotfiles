1368b9e27d059561ac9745e89dfdc675c0398e4e
miya2804 <32477854+miya2804@users.noreply.github.com>
2020-07-29 16:35:38 +0900

    Change face color of default.

--- ln/.emacs.d/init.el
+++ ln/.emacs.d/init.el
@@ -134,9 +134,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "color-236" :foreground "ghost white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default"))))
  '(font-lock-function-name-face ((t (:foreground "color-208"))))
  '(hl-line ((t (:background "gray25"))))
  '(web-mode-html-tag-bracket-face ((t (:foreground "white"))))
  '(web-mode-html-tag-face ((t (:foreground "brightblue")))))
-(set-face-background 'default "black")
+(set-face-background 'default "color-235")
 (set-face-foreground 'default "ghost white")
