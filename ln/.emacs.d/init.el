0dfded20409eda0bf891d8b98f124c7daabee1ca
miya2804 <32477854+miya2804@users.noreply.github.com>
2020-08-01 00:51:56 +0900

    add mozc settings.(apt install emacs-mozc and emacs-mozc-bin)

--- ln/.emacs.d/init.el
+++ ln/.emacs.d/init.el
@@ -26,14 +26,18 @@
 ;; ++++++++++++++++++++++++++++++++++++++++++++++++++
 ;; language settings
 ;; ++++++++++++++++++++++++++++++++++++++++++++++++++
+(require 'mozc)
+(set-language-environment "Japanese")
+(setq default-input-method "japanese-mozc")
+(prefer-coding-system 'utf-8)
+
 ;;(set-local-environment nil)
-;;(set-language-environment "Japanese")
 ;;(set-terminal-coding-system 'utf-8)
 ;;(set-keyboard-coding-system 'utf-8)
 ;;(set-buffer-file-coding-system 'utf-8)
 ;;(setq default-buffer-file-coding-system 'utf-8)
 ;;(set-default-coding-system 'utf-8)
-;;(prefer-coding-system 'utf-8)
+
 
 
 
@@ -119,7 +123,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (web-mode php-mode))))
+ '(package-selected-packages (quote (mozc web-mode php-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
