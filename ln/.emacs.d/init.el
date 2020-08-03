65d2cd04f769ff1d49ba4169201ce413da2597fa
miya2804 <32477854+miya2804@users.noreply.github.com>
2020-08-03 15:08:46 +0900

    add extension(.markdown) to load markdown-mode.

--- ln/.emacs.d/init.el
+++ ln/.emacs.d/init.el
@@ -140,9 +140,10 @@
 ;; ++++++++++++++++++++++++++++++++++++++++++++++++++
 ;;;; web-mode
 (require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.php?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
 
 ;;;; markdown-mode
 (require 'markdown-mode)
-(add-to-list 'auto-mode-alist '("\\.md?\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
