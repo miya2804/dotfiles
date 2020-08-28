1a1a1b3b2bbfc01dc62b79dc046cc381fa4993b4
miya2804 <32477854+miya2804@users.noreply.github.com>
2020-08-28 15:14:02 +0900

    起動時のwindow sizeを調整する関数(使っていなかった)を除去

--- ln/.emacs.d/init.el
+++ ln/.emacs.d/init.el
@@ -206,24 +206,6 @@
 
 ;;;; window size settings
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
-;; (defun set-frame-size-according-to-resolution ()
-;;   (interactive)
-;;   (if (display-graphic-p)
-;;   (progn
-;;     ;; use 120 char wide window for largeish displays
-;;     ;; and smaller 80 column windows for smaller displays
-;;     ;; pick whatever numbers make sense for you
-;;     (if (> (x-display-pixel-width) 1280)
-;;            (add-to-list 'default-frame-alist (cons 'width 120))
-;;            (add-to-list 'default-frame-alist (cons 'width 50)))
-;;     ;; for the height, subtract a couple hundred pixels
-;;     ;; from the screen height (for panels, menubars and
-;;     ;; whatnot), then divide by the height of a char to
-;;     ;; get the height we want
-;;     (add-to-list 'default-frame-alist
-;;          (cons 'height (/ (- (x-display-pixel-height) 200)
-;;                              (frame-char-height)))))))
-;; (set-frame-size-according-to-resolution)
 
 ;;;; display-time
 ;; (setq display-time-day-and-date nil)
