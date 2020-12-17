2ce14e0c24b0883e5f101c14eb827326ca541482
miya2804 <32477854+miya2804@users.noreply.github.com>
2020-12-17 20:56:50 +0900

    Change the command of the message when use-package is not available.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -162,7 +162,7 @@ If there are multiple windows, the 'other-window' is called."
     (progn
       ;; when use-package is not available
       (when (null (require 'use-package nil t))
-        (warn "`use-package' is unavailable!  Please install it via `M-x list-packages' if possible.")
+        (warn "`use-package' is unavailable!  Please install it via `M-x package-list-packages' if possible.")
         (defmacro use-package (&rest _args))))))
 
 ;; 後の startup.el におけるオプション認識エラーを防止
