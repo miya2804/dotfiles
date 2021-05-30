8feac5000720b6c10476f1122e625219042414e5
mmugi <32477854+mmugi@users.noreply.github.com>
2021-05-30 13:24:13 +0900

    Minor update

--- fish/functions/__fzf_git_add.fish
+++ fish/functions/__fzf_git_add.fish
@@ -5,7 +5,7 @@ function __fzf_git_add --description 'Use fzf to interactively add modified cont
     end
 
     if functions -q __fzf_preview_git_diff
-        git status --short | awk '{if (substr($0,2,1) !~ / /) print $0}' | \
+        command git status --short | awk '{if (substr($0,2,1) !~ / /) print $0}' | \
           fzf --height 90% --prompt 'GIT ADD > ' --exit-0 \
           --preview "__fzf_preview_git_diff {}" \
           --preview-window=right:60%:wrap \
--- fish/functions/__fzf_preview_git_diff.fish
+++ fish/functions/__fzf_preview_git_diff.fish
@@ -1,13 +1,13 @@
-function __fzf_preview_git_diff
+function __fzf_preview_git_diff --description 'Show changes between commits for fzf preview using sentence of git status --short.'
     if not command -sq git
         __echo_error '__fzf_preview_git_diff: git command not found.'
         return 1
     end
 
-    set -l git_status (echo $argv | awk '{print $1}')
+    set -l git_status_short_format (echo $argv | awk '{print substr($0,1,2)}')
     set -l file (echo $argv | awk '{print $2}')
 
-    if test "$git_status" = "??"
+    if test "$git_status_short_format" = "??"
         echo (set_color red)'Untracked file.'(set_color normal)
         echo '----'
         cat $file
