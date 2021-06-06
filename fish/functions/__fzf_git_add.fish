780670a24d9296591806d6850eef37aba2395494
mmugi <32477854+mmugi@users.noreply.github.com>
2021-06-07 01:41:25 +0900

    Minor update

--- fish/functions/__fzf_git_add.fish
+++ fish/functions/__fzf_git_add.fish
@@ -6,21 +6,21 @@ function __fzf_git_add --description 'Use fzf to interactively add modified cont
 
     if functions -q __fzf_preview_git_diff
         command git status --short | awk '{if (substr($0,2,1) !~ / /) print $0}' | \
-          fzf --height 100% --prompt 'GIT ADD > ' --exit-0 \
+        fzf --height 100% --prompt 'GIT ADD > ' --exit-0 \
           --preview "__fzf_preview_git_diff {}" \
           --preview-window=down:85%:wrap \
-          --bind "$fzf_preview_bind" | awk '\
-            {
-              if (substr($0,1,2) !~ /R/) {\
-                print $2 \
-              } else {
-                print $4 \
-              }
-            }' | \
-          while read staged_file
-              echo 'Staged: '$staged_file
-              command git add $staged_file
-          end
+          --bind "$fzf_preview_bind" | \
+        awk '{
+               if (substr($0,1,2) !~ /R/) {\
+                 print $2 \
+               } else {
+                 print $4 \
+               }
+             }' | \
+        while read staged_file
+            echo 'Staged: '$staged_file
+            command git add $staged_file
+        end
     else
         __echo_error '__fzf_git_add: __fzf_preview_git_diff command not found.'
         return 1
