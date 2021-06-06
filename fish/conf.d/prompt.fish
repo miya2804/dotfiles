a6f6a3fe71f2aa6aef70f290743750db84d62939
mmugi <32477854+mmugi@users.noreply.github.com>
2021-06-06 21:53:17 +0900

    Minor update

--- fish/conf.d/prompt.fish
+++ fish/conf.d/prompt.fish
@@ -1,17 +1,26 @@
-### variables
+### PROMPT ###
+
+# tide
+set tide_prompt_char_success_color normal
+set tide_prompt_char_icon '>'
+set tide_time_format '%H:%M'
+
+### GIT PROMPT ###
+
 # prefix of the git branch name is show or not
 set space_branch_prefix true
 
-# git prompt color
-# branch name
+# branch name color
 set fish_color_git_branch_detached brmagenta
 set fish_color_git_clean green
 set fish_color_git_staged yellow
 set fish_color_git_dirty red
-# action
+
+# action color
 set fish_color_git_commit_hash yellow
 set fish_color_git_action brred
-# info
+
+# info color
 set fish_color_git_upstream normal
 set fish_color_git_ahead brmagenta
 set fish_color_git_behind brmagenta
@@ -23,7 +32,7 @@ set fish_color_git_renamed cyan
 set fish_color_git_unmerged yellow
 set fish_color_git_untracked white
 
-# git prompt character
+# status character
 set fish_prompt_git_status_stashed '$'
 set fish_prompt_git_status_modified '*'
 set fish_prompt_git_status_added '+'
@@ -35,19 +44,12 @@ set fish_prompt_git_status_upstream '='
 set fish_prompt_git_status_behind '<'
 set fish_prompt_git_status_ahead '>'
 
-### plugin settings
-# tide
-# color setting
-set tide_prompt_char_success_color normal
-set tide_prompt_char_icon '>'
-set tide_time_format '%H:%M'
-
-# set git information of tide prompt
-if functions -q tide _tide_item_git
-    function _tide_item_git
-        set -l git_status (my_fish_git_prompt)
-        test -n "$git_status"
-        and echo (set_color brblack)\["$git_status"(set_color brblack)\](set_color normal)
-    end
+# set git informations of tide
+if functions -q tide
     set space_branch_prefix false
+    function _tide_item_git
+        set -l git_prompt (my_fish_git_prompt)
+        test -n "$git_prompt"
+        and echo (set_color brblack)\["$git_prompt"(set_color brblack)\](set_color normal)
+    end
 end
